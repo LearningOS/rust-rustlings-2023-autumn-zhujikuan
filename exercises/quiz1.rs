62888c9a43b7443ab216f4cf620726da6f207017
xiang <1335289286@qq.com>
2023-10-09 12:36:45 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,15 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: i32) -> i32 {
+    let mut price = 0;
+    if cnt > 40 {
+        price = cnt;
+    } else {
+        price = cnt * 2;
+    }
+    price
+}
 
 // Don't modify this function!
 #[test]
